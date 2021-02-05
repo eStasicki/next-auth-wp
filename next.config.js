6645182ac00286c0b0e4e3112f269183646fa56b
Ernest Stasicki <estasicki@gmail.com>
2021-02-05 20:24:57 +0100

    Add .ENV

--- next.config.js
+++ next.config.js
@@ -1,15 +1,30 @@
 module.exports = {
-  webpack: (config, { dev }) => {
-    if (dev) {
-      config.module.rules.push({
-        test: /\.js$/,
-        exclude: /node_modules/,
-        //loader: 'eslint-loader',
-        // options: {
-        //   // eslint options (if necessary)
-        // },
+
+  devIndicators: {
+    autoPrerender: false,
+  },
+
+  webpack: (config) => {
+    const oneOf = config.module.rules.find(
+      (rule) => typeof rule.oneOf === 'object'
+    );
+
+    const fixUse = (use) => {
+      if (use.loader.indexOf('css-loader') >= 0 && use.options.modules) {
+        use.options.modules.mode = 'local';
+      }
+    };
+
+    if (oneOf) {
+      oneOf.oneOf.forEach((rule) => {
+        if (Array.isArray(rule.use)) {
+          rule.use.map(fixUse);
+        } else if (rule.use && rule.use.loader) {
+          fixUse(rule.use);
+        }
       });
     }
+
     return config;
   },
 };
