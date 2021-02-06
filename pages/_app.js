8ec1988db0d87006151983999ae227bebd688d90
Ernest Stasicki <ernest.stasicki@monogo.pl>
2021-02-06 20:59:10 +0100

    clear project

--- pages/_app.js
+++ pages/_app.js
@@ -9,35 +9,31 @@ import withRedux from 'next-redux-wrapper';
 import { initStore } from '../redux';
 import { API_GRAPHQL } from '../config';
 
-
 const client = new ApolloClient({
   uri: API_GRAPHQL,
-  fetch: fetch
+  fetch: fetch,
 });
 
 export default withRedux(initStore, { debug: false })(
   class MyApp extends App {
     static async getInitialProps({ Component, ctx }) {
-      
       return {
         pageProps: {
           ...(Component.getInitialProps
             ? await Component.getInitialProps(ctx)
-            : {})
-        }
+            : {}),
+        },
       };
     }
 
     render() {
       const { Component, pageProps, store } = this.props;
       return (
-        <Container>
-          <Provider store={store}>
-            <ApolloProvider client={client}>
+        <Provider store={store}>
+          <ApolloProvider client={client}>
             <Component {...pageProps} />
-            </ApolloProvider>
-          </Provider>
-        </Container>
+          </ApolloProvider>
+        </Provider>
       );
     }
   }
--- redux/reducers/authReducer.js
+++ redux/reducers/authReducer.js
@@ -4,13 +4,15 @@ const initialState = {
   token: null,
 };
 
-export default (state = initialState, action) => {
-  switch(action.type) {
-  case AUTHENTICATE:
-    return { token: action.payload };
-  case DEAUTHENTICATE:
-    return { token: null };
-  default:
-    return state;
+const AuthReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case AUTHENTICATE:
+      return { token: action.payload };
+    case DEAUTHENTICATE:
+      return { token: null };
+    default:
+      return state;
   }
 };
+
+export default AuthReducer;
