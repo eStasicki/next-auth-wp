c21707095e0857cd3217af7c99782bc0cd0c0926
Ernest Stasicki <ernest.stasicki@monogo.pl>
2021-02-08 14:42:49 +0100

    Repair import image from Public

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import initialize from '@utils/initialize';
 const Index = () => (
   <Layout title="Home">
     <h2 className="title is-2">Authentication with Next.js and JWT</h2>
-    <img src="/public/nextjs.jpg" />
+    <img src="/nextjs.jpg" />
     <p>
       A proof of concept app, demonstrating the authentication of Next.js
       application using JWT.
