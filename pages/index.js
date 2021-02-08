599296fac1d43b64ad125c47a7668f428199d70b
Ernest Stasicki <ernest.stasicki@monogo.pl>
2021-02-08 14:39:31 +0100

    Change static to public

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import initialize from '@utils/initialize';
 const Index = () => (
   <Layout title="Home">
     <h2 className="title is-2">Authentication with Next.js and JWT</h2>
-    <img src="/static/nextjs.jpg" />
+    <img src="/public/nextjs.jpg" />
     <p>
       A proof of concept app, demonstrating the authentication of Next.js
       application using JWT.
