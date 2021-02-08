682de5cd866f0e58d9d8cbd5fc959f133ecc4a5a
Ernest Stasicki <ernest.stasicki@monogo.pl>
2021-02-08 10:54:57 +0100

    add eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,19 @@
 module.exports = {
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+      experimentalObjectRestSpread: true,
+    },
+  },
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+    jest: true,
+  },
   plugins: ['simple-import-sort'],
 };
